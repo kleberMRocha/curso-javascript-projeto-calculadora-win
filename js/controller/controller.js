27e1623369dbcd46565388e2bd7f3da6a21d407d
kleber Rocha <kleberrocha2701@gmail.com>
2020-05-16 11:08:33 -0300

    Addition of percentage function

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -2,5 +2,6 @@ let $ = document.querySelector.bind(document);
 let $$ = document.querySelectorAll.bind(document);
 
 let display = $("#display");
+let lastOperation = $('[data-display]')
 let btns = $$("[data-btn]");
-let calc = new Calc(display,btns);
+let calc = new Calc(display,btns,lastOperation);
--- js/models/models.js
+++ js/models/models.js
@@ -1,10 +1,14 @@
 class Calc{
-    constructor(display,btns){
+    constructor(display,btns,lastOperationDisplay){
 
         this.btns = btns;
         this.display = display;
         this._displayContent = [];
         this.addEventClick(this.btns);
+        this.lastOperationDisplay = lastOperationDisplay;
+        this.lastOperation =[];
+        this.regex = [/\,/gm,/\./gm,/\/100\*/gm];
+       
     }
 
     get displayContent(){
@@ -16,48 +20,46 @@ class Calc{
         this._displayContent = value;
     }
 
-    //remove the commas from an array and add it to the display
-    setDisplayContent(){
-
-        this.display.innerHTML = this.displayContent;
-        let whithOutComma = this.display.innerHTML.replace(/\,/gm,"");
-        this.display.innerHTML = whithOutComma;
-        
-    }
-
     //add "click" event for all buttons
-    addEventClick(value){
+       addEventClick(value){
 
         value.forEach(element => {
 
             element.addEventListener("click",event =>{
 
-                
+                this.isAnumber(this.lastOperation,this.displayContent,element.dataset.btn);
+
                 switch (element.dataset.btn) {
+                    case "CE": // clear all
+                        this._displayContent.pop();
+                        this.setDisplayContent();
+
+                        break;
+                        
                     case "C": // clear all
                         this.resetDisplay();
                         break;
 
-                    case "CE": // clear all
-                        this.resetDisplay();
+                    case 'percent': // clear all
+        
+                        this._displayContent.push("/100*");
+                        this.setDisplayContent();
+
+
                         break;
 
                     case "backspace": // deletes the last input
-
                         this._displayContent.pop();
                         this.setDisplayContent();
 
                         break;
                     case "=": //eval the values on the display
-                        
-                       let resultado = eval(this.display.innerHTML);
-                       this.resetDisplay();
-                       this._displayContent.push(resultado)
-                       this.setDisplayContent();
+
+                        this.equationSolver();
                             
                         break;
                 
-                    default: //resolves the operation shown on the display
+                    default: //resolves the operation and shown on the display
                         this._displayContent.push(element.innerHTML);
                         this.setDisplayContent();
                         break;
@@ -69,12 +71,71 @@ class Calc{
         
     }
 
+
+    // solve the equations
+    // convert comma to period
+    // convert the percent value
+    equationSolver(){
+
+        this.lastOperation.push(this.display.innerHTML);
+        let resultado = eval(this.display.innerHTML.replace(this.regex[0],".")
+        .replace("%","/100*"));
+
+        this.setDisplayLastOperation(this.lastOperationDisplay,this.lastOperation,resultado);
+        this.resetDisplay();
+        
+        this._displayContent.push(String(resultado).replace(this.regex[1],","));
+        this.setDisplayContent();
+
+    }
+
+
+    //add it to the display without array commas
+    setDisplayContent(){
+
+        this.display.innerHTML = this.displayContent.join("").replace(this.regex[2],"%");
+        
+    }
+
+ 
+    //add  the last operation to the display 
+    setDisplayLastOperation(lastOperationDisplay,lastOperation,resultado){
+        lastOperationDisplay.innerHTML = 
+        `Resultado: ${lastOperation[lastOperation.length-1]} = ${resultado}`;
+    }
+
     //reset display and set a default value
     resetDisplay(){
-  
         this.display.innerHTML = "0";
         this.displayContent = [];
+       
+    }
 
+    //checks whether the next input is an operator or a number
+    isAnumber(lastOperation,displayContent,buttonsType){
+
+        if(lastOperation.length == 1 && displayContent.length == 1){
+            
+            switch (buttonsType) {
+                case "number":
+                     //does not allow join()
+                    this._displayContent.shift();
+                    this.setDisplayContent();
+                    this.lastOperation =[];
+                    break;
+
+                case "operador":
+                    //allow the join()
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        }
+
+        
     }
 
     
