3ae6f8be8393ca9658c7744dd483ad45fc3ee8d5
kleber Rocha <kleberrocha2701@gmail.com>
2020-05-17 07:30:28 -0300

    new functions, bug fixes

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -2,6 +2,7 @@ let $ = document.querySelector.bind(document);
 let $$ = document.querySelectorAll.bind(document);
 
 let display = $("#display");
-let lastOperation = $('[data-display]')
+let lastOperation = $('[data-display="lastOperation"]');
+let displayError = $('[data-display="error"]');
 let btns = $$("[data-btn]");
-let calc = new Calc(display,btns,lastOperation);
+let calc = new Calc(display,btns,lastOperation,displayError);
--- js/models/models.js
+++ js/models/models.js
@@ -1,13 +1,14 @@
 class Calc{
-    constructor(display,btns,lastOperationDisplay){
+    constructor(display,btns,lastOperationDisplay,displayError){
 
         this.btns = btns;
         this.display = display;
+        this.displayError = displayError;
         this._displayContent = [];
         this.addEventClick(this.btns);
         this.lastOperationDisplay = lastOperationDisplay;
         this.lastOperation =[];
-        this.regex = [/\,/gm,/\./gm,/\/100\*/gm];
+        this.regex = [/\,/gm,/\./gm,/\/100\*/gm,/[\/*-+]/gm];
        
     }
 
@@ -28,35 +29,38 @@ class Calc{
             element.addEventListener("click",event =>{
 
                 this.isAnumber(this.lastOperation,this.displayContent,element.dataset.btn);
+                this.maxLengthDisplay(this.display.textContent,this.displayError);
 
                 switch (element.dataset.btn) {
                     case "CE": // clear all
-                        this._displayContent.pop();
-                        this.setDisplayContent();
-
+                        this.ceFun(this.displayContent,this.display);
                         break;
                         
                     case "C": // clear all
                         this.resetDisplay();
+                        this.resetLastOperation();
+                        break;
+
+                    case "sign": // clear all
+                    
+                       this.NewSign(this.display.innerHTML,this.displayContent,this.regex[3]);
+
                         break;
 
                     case 'percent': // clear all
         
                         this._displayContent.push("/100*");
                         this.setDisplayContent();
-
-
                         break;
 
                     case "backspace": // deletes the last input
-                        this._displayContent.pop();
-                        this.setDisplayContent();
+
+                         this.ceFun(this.displayContent,this.display);
 
                         break;
                     case "=": //eval the values on the display
 
-                        this.equationSolver();
-                            
+                        this.equationSolver();   
                         break;
                 
                     default: //resolves the operation and shown on the display
@@ -77,7 +81,12 @@ class Calc{
     // convert the percent value
     equationSolver(){
 
+        if(this.lastOperation.length > 0){
+            this.lastOperation.shift();
+        }
+
         this.lastOperation.push(this.display.innerHTML);
+
         let resultado = eval(this.display.innerHTML.replace(this.regex[0],".")
         .replace("%","/100*"));
 
@@ -86,7 +95,7 @@ class Calc{
         
         this._displayContent.push(String(resultado).replace(this.regex[1],","));
         this.setDisplayContent();
-
+        
     }
 
 
@@ -97,9 +106,9 @@ class Calc{
         
     }
 
- 
     //add  the last operation to the display 
     setDisplayLastOperation(lastOperationDisplay,lastOperation,resultado){
+
         lastOperationDisplay.innerHTML = 
         `Resultado: ${lastOperation[lastOperation.length-1]} = ${resultado}`;
     }
@@ -108,11 +117,18 @@ class Calc{
     resetDisplay(){
         this.display.innerHTML = "0";
         this.displayContent = [];
+        
        
+    }
+    //reset display Last Operation and set a default value
+    resetLastOperation(){
+        this.lastOperationDisplay.innerHTML = "-" ;
+        this.lastOperation =[];
     }
 
     //checks whether the next input is an operator or a number
     isAnumber(lastOperation,displayContent,buttonsType){
+        
 
         if(lastOperation.length == 1 && displayContent.length == 1){
             
@@ -120,8 +136,9 @@ class Calc{
                 case "number":
                      //does not allow join()
                     this._displayContent.shift();
+                    this.lastOperation.shift();
                     this.setDisplayContent();
-                    this.lastOperation =[];
+                    
                     break;
 
                 case "operador":
@@ -132,10 +149,86 @@ class Calc{
                     break;
             }
 
-
         }
 
         
+    }
+
+    //check max display length
+    maxLengthDisplay(value,displayError){
+    
+       if(value.length > 41){
+        displayError.innerHTML = "Numero maximo de caracteres excedido!"
+        this.resetDisplay();
+        this.resetLastOperation();
+        setTimeout(()=>{
+            this.resetDisplay();
+        },30);
+      
+       }else{
+        displayError.innerHTML = "";
+       }
+
+    }
+
+ 
+    //CE Button
+    ceFun(displayContent){
+
+        displayContent.pop();
+        this.resetLastOperation();
+        this.setDisplayContent();
+
+        if(display.innerHTML.length == 0){
+         display.innerHTML = 0;
+        }
+
+
+    }
+
+
+    NewSign(display,displayContent,regex){
+
+        //transforms the display value into an array
+        let displayValue = display.split(/[\//*-+%]/gm);
+        //checks the size of the last inputted number
+        let subArray = displayValue[displayValue.length -1].length;
+
+             //check whether a number is positive, negative or zero
+            switch ((Math.sign(displayValue[displayValue.length -1]))) {
+                case 1:
+                    //removes the old values, so the new value can now be inserted
+                    for(let i=0;i < subArray;i++){
+
+                        if(regex.test(displayContent[displayContent.length-1])== true){
+                            displayContent.push(`-${displayValue[displayValue.length -1]}`);
+                            this.setDisplayContent();
+                            return;
+                        }else{
+                            
+                            displayContent.pop();  
+                        };  
+                    }
+
+                    displayContent.push(`-${displayValue[displayValue.length -1]}`);
+                    this.setDisplayContent();
+                    
+                    break;
+                case -1:
+
+                    displayContent.pop();
+                    displayContent.push(`${displayValue[displayValue.length -1].replace(/[-()]/gm,"")}`);
+                    this.setDisplayContent();
+                
+                break;
+                case 0:
+                    
+                break;
+            
+                default:
+                    break;
+            }        
+ 
     }
 
     
