09656a976b7115e69fe1773d348e83123f9f3437
kleber Rocha <kleberrocha2701@gmail.com>
2020-05-19 05:42:14 -0300

    functions completed and  bug fixe

--- js/models/models.js
+++ js/models/models.js
@@ -8,7 +8,7 @@ class Calc{
         this.addEventClick(this.btns);
         this.lastOperationDisplay = lastOperationDisplay;
         this.lastOperation =[];
-        this.regex = [/\,/gm,/\./gm,/\/100\*/gm,/[\/*-+]/gm];
+        this.regex = [/\,/gm,/\./gm,/\/100\*/gm,/[\/*-+%]/gm];
        
     }
 
@@ -30,6 +30,7 @@ class Calc{
 
                 this.isAnumber(this.lastOperation,this.displayContent,element.dataset.btn);
                 this.maxLengthDisplay(this.display.textContent,this.displayError);
+              
 
                 switch (element.dataset.btn) {
                     case "CE": // clear all
@@ -43,7 +44,7 @@ class Calc{
 
                     case "sign": // clear all
                     
-                       this.NewSign(this.display.innerHTML,this.displayContent,this.regex[3]);
+                       this.NewSign(this.display.innerHTML,this.displayContent,this.regex);
 
                         break;
 
@@ -52,6 +53,26 @@ class Calc{
                         this._displayContent.push("/100*");
                         this.setDisplayContent();
                         break;
+
+                    case "sqrt": //eval the values on the display
+
+                        this.sqrt(this.display.innerHTML,this.displayContent,this.regex);   
+
+                    break;
+
+                    case "pow": //eval the values on the display
+
+                    this.pow(this.display.innerHTML,this.displayContent,this.regex);   
+
+                    break;
+                
+                    
+                    case "reciproc": //eval the values on the display
+
+                    this.reciproc(this.display.innerHTML,this.displayContent,this.regex);   
+
+                    break;
+
 
                     case "backspace": // deletes the last input
 
@@ -75,7 +96,6 @@ class Calc{
         
     }
 
-
     // solve the equations
     // convert comma to period
     // convert the percent value
@@ -94,14 +114,15 @@ class Calc{
         this.resetDisplay();
         
         this._displayContent.push(String(resultado).replace(this.regex[1],","));
-        this.setDisplayContent();
-        
-    }
 
+        this.setDisplayContent();
+
+    }
 
     //add it to the display without array commas
     setDisplayContent(){
 
+        if(this.displayContent.includes("NaN"))this.resetDisplay(),this.displayError.innerHTML = "Operação Invalida!";
         this.display.innerHTML = this.displayContent.join("").replace(this.regex[2],"%");
         
     }
@@ -126,7 +147,7 @@ class Calc{
         this.lastOperation =[];
     }
 
-    //checks whether the next input is an operator or a number
+    //checks whether the next input is an operator or a number  
     isAnumber(lastOperation,displayContent,buttonsType){
         
 
@@ -152,6 +173,28 @@ class Calc{
         }
 
         
+    }
+
+    //removes the old values, so the new value can now be inserted
+    //prevents operators from being removed
+    preventsPopOperators(Sub,regex,displayContent,displayValue){
+        
+        for(let i=0;i < Sub;i++){
+            if(regex[3].test(this.LastArrayValue(displayContent))== true){
+                displayContent.push(-1*this.LastArrayValue(displayValue));
+                this.setDisplayContent();
+                return;
+            }else{
+                displayContent.pop();  
+            };  
+        }
+    }
+
+
+    LastArrayValue(array){
+
+       return array[array.length -1]
+
     }
 
     //check max display length
@@ -186,49 +229,104 @@ class Calc{
 
     }
 
-
+   
+    // ± Button
     NewSign(display,displayContent,regex){
 
         //transforms the display value into an array
         let displayValue = display.split(/[\//*-+%]/gm);
+        let pushValue = -1*(this.LastArrayValue(displayValue)).replace(regex[0],".");
+
+        this.applyFunction(display,displayContent,regex,pushValue);
+              
+      
+    }
+
+    // √ Button
+
+    sqrt(display,displayContent,regex){
+       
+       //transforms the display value into an array
+       let displayValue = display.split(/[\//*-+%]/gm); 
+       let pushValue = Math.sqrt(this.LastArrayValue(displayValue).replace(regex[0],".")).toFixed(2);
+
+       this.applyFunction(display,displayContent,regex,pushValue);
+
+      
+                      
+                    
+    }
+
+    //pow Button
+
+    pow(display,displayContent,regex){
+       
+        //transforms the display value into an array
+        let displayValue = display.split(/[\//*-+%]/gm);
+        let pushValue = Math.pow(this.LastArrayValue(displayValue).replace(regex[0],"."),2);
+
+        this.applyFunction(display,displayContent,regex,pushValue);
+      
+                         
+     }
+
+     //¹/x button
+
+    reciproc(display,displayContent,regex){
+
+       //transforms the display value into an array
+       let displayValue = display.split(/[\//*-+%]/gm);
+       if(this.LastArrayValue(displayValue) == 0){
+        
+        this.displayError.innerHTML = "Operação Invalida!";
+        this.resetDisplay();
+        
+       }
+
+       let pushValue = 1/(this.LastArrayValue(displayValue).replace(regex[0],"."));
+
+       this.applyFunction(display,displayContent,regex,pushValue);
+                   
+     }
+
+
+    
+    applyFunction(display,displayContent,regex,pushValue){
+
+        //transforms the display value into an array
+        let displayValue = display.split(/[\//*-+%]/gm);
         //checks the size of the last inputted number
-        let subArray = displayValue[displayValue.length -1].length;
-
-             //check whether a number is positive, negative or zero
-            switch ((Math.sign(displayValue[displayValue.length -1]))) {
-                case 1:
-                    //removes the old values, so the new value can now be inserted
-                    for(let i=0;i < subArray;i++){
-
-                        if(regex.test(displayContent[displayContent.length-1])== true){
-                            displayContent.push(`-${displayValue[displayValue.length -1]}`);
-                            this.setDisplayContent();
-                            return;
-                        }else{
-                            
-                            displayContent.pop();  
-                        };  
-                    }
-
-                    displayContent.push(`-${displayValue[displayValue.length -1]}`);
-                    this.setDisplayContent();
-                    
-                    break;
-                case -1:
-
-                    displayContent.pop();
-                    displayContent.push(`${displayValue[displayValue.length -1].replace(/[-()]/gm,"")}`);
-                    this.setDisplayContent();
-                
-                break;
-                case 0:
-                    
-                break;
-            
-                default:
-                    break;
-            }        
+        let subArray = this.LastArrayValue(displayValue).length;
  
+       
+             //check whether a number is positive, negative 
+ 
+                for(let i=0;i < subArray;i++){
+ 
+                    //is the value a operator?
+                    if(regex[3].test(this.LastArrayValue(displayContent))== true){
+                       
+                        //converts a number to positive or negative
+                        // replace commas to dots so then can be used by eval() method
+                        let withoutComma = (pushValue);
+ 
+                        displayContent.push(String(withoutComma).replace(regex[1],","));
+                        this.setDisplayContent();
+                        return;
+ 
+                    }else{
+                        
+                        displayContent.pop();  
+                    };  
+                }
+              
+                let withoutComma = (pushValue);
+                displayContent.push(String(withoutComma).replace(regex[1],","));
+                this.setDisplayContent();    
+
+
+
+
     }
 
     
